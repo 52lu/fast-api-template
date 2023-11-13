dc13384b75a34855a965bc8b6a2c2e11d5f9a8a8
liuqinghui <liuqinghui@weimiao.cn>
2023-11-13 19:28:49 +0800

    deving

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
-# 临时启动脚本
-uvicorn app.main:app --host 0.0.0.0 --port 9000 --workers 4
+# 本地测试发布，临时启动脚本
+/root/anaconda3/envs/py310/bin/uvicorn app.main:app --host 0.0.0.0 --port 9000 --workers 4
